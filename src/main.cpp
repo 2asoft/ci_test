0cb63a3b7ee6ad86eecbc23cf1993fe38ed016fe
Anton Afanasyev <aasoft@gmail.com>
2020-06-28 02:01:14 -0700

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 
+void foof(int i, int j, int k, int lmnop, int kldjfghdskfjlghdfkjggdjlfkhgdfk, int foo, int bar, int baz, double triple,
+		  float ing) {
+	if (i == 42) {
+		std::cout << 1 << std::endl;
+	} else {
+		if (i == 43) {
+			std::cout << 2 << std::endl;
+		}
+		std::cout << 3 << std::endl;
+	}
+}
 int main() {
 	std::cout << "Hello, World!" << std::endl;
 	int foo = 42;
 
-	std::count << foo << std::endl;
+	std::cout << foo << std::endl;
 
 	return 0;
 }
